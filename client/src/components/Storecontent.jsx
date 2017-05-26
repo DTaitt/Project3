99e180099310e3037f5b25ebde428eaa4a434cc6
DTaitt <donovan.taitt@yahoo.com>
2017-05-26 14:48:31 -0400

    minor fixes for accessability

--- client/src/components/Storecontent.jsx
+++ client/src/components/Storecontent.jsx
@@ -4,10 +4,10 @@ class Storecontent extends Component{
   render(){
     return(
       <div className="Div-Storecontent">
-        <h2 className="store_name">Store Name</h2>
+        <h1 className="store_name">Store Name</h1>
         <img src="http://www.danielarestaurant.com/images/pnp2.jpg" alt="Pic from api" /> 
         <div className="Store-content-text">
-          <p className="store_address"> 100 BroadwayStreet, New York, NY</p>
+          <h2 className="store_address"> 100 BroadwayStreet, New York, NY</h2>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an </p>
           <p>unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
           </div>
