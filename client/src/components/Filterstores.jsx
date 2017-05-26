db82eb1dc57310966277e87fc275da4b2b577a2e
DTaitt <donovan.taitt@yahoo.com>
2017-05-26 12:58:45 -0400

    refactor components

--- client/src/components/Filterstores.jsx
+++ client/src/components/Filterstores.jsx
@@ -9,21 +9,20 @@ class Filterstores extends Component{
   render(){
    return(
       <div>
-          <div className="Navthreeoptions">
-               <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
-                <Tab eventKey={1} title="Food" className="Foodtab">
-                  <Storelist/>
-                </Tab>
-                <Tab eventKey={2} title="Nightlife" className="Nighttab">
-                  <Storelist/> 
-                </Tab>
-                <Tab eventKey={3} title="Fashion" className="Fashiontab">
-                  <Storelist/>
-                </Tab>
-              </Tabs>
-          </div>
-        
-        </div>     
+        <div className="Navthreeoptions">
+          <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+            <Tab eventKey={1} title="Food" className="Foodtab">
+              <Storelist/>
+            </Tab>
+            <Tab eventKey={2} title="Nightlife" className="Nighttab">
+              <Storelist/> 
+            </Tab>
+            <Tab eventKey={3} title="Fashion" className="Fashiontab">
+              <Storelist/>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>     
    );
   }
 }
--- client/src/components/Storecontent.jsx
+++ client/src/components/Storecontent.jsx
@@ -4,9 +4,10 @@ class Storecontent extends Component{
   render(){
     return(
       <div className="Div-Storecontent">
+        <h2 className="store_name">Store Name</h2>
         <img src="http://www.danielarestaurant.com/images/pnp2.jpg" alt="Pic from api" /> 
         <div className="Store-content-text">
-          <h2> 100 BroadwayStreet, New York, NY</h2>
+          <p className="store_address"> 100 BroadwayStreet, New York, NY</p>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an </p>
           <p>unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
           </div>
